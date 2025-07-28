aaeb6247579111c3a7bea93225d2e7d3a2480213
hasipersonal <vilochanahasith@gmai.com>
2025-07-28 21:04:10 +0530

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,193 @@
+import PropTypes from "prop-types"
+import { Icon } from "@iconify/react";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import { useState } from "react";
+
+
+const Contact = ({ data }) => {
+  const { contactInfo, contactForm } = data;
+
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  // Handler for input field changes
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const formData = new FormData(event.target);
+    formData.append("access_key", "fcc74231-656a-425b-a54f-aff38354fadb");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      setFormData({ name: "", email: "", subject: "", message: "" });
+      setLoading(false)
+    }
+  };
+  return (
+    <section
+      id="contact"
+      data-scroll-index={5}
+      className="section contact-section"
+    >
+      <div className="container">
+        <div className="row gy-5">
+          <div className="col-lg-5">
+            <SectionHeading title="Reach out me" subTitle="Contact" />
+            <div className="contact-info">
+              <ul>
+                {contactInfo.map((element, index) => (
+                  <li key={index} data-aos="fade-up" data-aos-duration="800" data-aos-delay="400">
+                    <div className="icon">
+                      <Icon icon={`bi:${element.icon}`} />
+                    </div>
+                    <div className="text">
+                      <label>{element.title}</label>
+                      <p>
+                        {element.text}
+                        <span>
+                          {element.emailLink &&
+                            <a
+                              className="text-reset"
+                              href="mailto:info@domainname.com"
+                            >
+                              {element.emailLink}
+                            </a>
+                          }
+                        </span>
+                      </p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              <div className="google-map" data-aos="fade-up" data-aos-duration="800" data-aos-delay="500">
+                <div className="ratio ratio-21x9">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62999.5206964189!2d79.87989330752542!3d6.909881614089015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2595d7a9d852d%3A0x82943f76793efceb!2sLakshmans%20Building%2C%20321%20Galle%20-%20Colombo%20Rd%2C%20Colombo%2000300!5e1!3m2!1sen!2slk!4v1753623805765!5m2!1sen!2slk"
+                    allowFullScreen=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-7 ps-xl-5">
+            <div className="contact-form bg-g" data-aos="fade-left" data-aos-duration="800" data-aos-delay="1000">
+              <div className="contact-head">
+                <h4>{contactForm.title}</h4>
+                <p>{contactForm.text}</p>
+              </div>
+              <form onSubmit={onSubmit} id="contact-form" method="POST">
+                <input type="hidden" name="from_name" value="Lilon Macwan" />
+                <input type="hidden" name="replyto" value="custom@gmail.com" />
+                <div className="row gx-3 gy-4">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label className="form-label">First name</label>
+                      <input
+                        name="name"
+                        id="name"
+                        placeholder="Name *"
+                        className="form-control"
+                        type="text"
+                        onChange={handleInputChange}
+                        value={formData.name}
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label className="form-label">Your Email</label>
+                      <input
+                        name="email"
+                        id="email"
+                        placeholder="Email *"
+                        className="form-control"
+                        type="email"
+                        onChange={handleInputChange}
+                        value={formData.email}
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <div className="form-group">
+                      <label className="form-label">Subject</label>
+                      <input
+                        name="subject"
+                        id="subject"
+                        placeholder="Subject *"
+                        className="form-control"
+                        type="text"
+                        onChange={handleInputChange}
+                        value={formData.subject}
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                    <div className="form-group">
+                      <label className="form-label">Your message</label>
+                      <textarea
+                        name="message"
+                        id="message"
+                        placeholder="Your message *"
+                        rows={6}
+                        className="form-control"
+                        onChange={handleInputChange}
+                        value={formData.message}
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                    <div className="send">
+                      <button
+                        className="px-btn dark w-100"
+                        type="submit"
+                        value="Send"
+                      >
+                        {loading ? "Sending..." : "Send Message"}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+Contact.propTypes = {
+  data: PropTypes.object
+}
+
+
+
+export default Contact;
